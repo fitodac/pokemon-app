1e7077bb8fd6136f2a7709e9f7836b32dcc6b30e
fitodac <fitodac@gmail.com>
2023-03-29 05:25:39 -0300

    Loading funny messages

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,11 +1,43 @@
+import { useState, useEffect } from 'react'
 import src from '../../assets/img/loading.gif'
 import style from './Loading.module.css'
 
+const messages = [
+	'...atrapando nuevos Pokémons',
+	'...entrenando a Ivysaur',
+	'...lustrando las Pokeballs',
+	'...dame unos segundos más por favor',
+	'Se está demorando un poquito, pero valdrá la pena',
+	'...buscando la gorra de Ash'
+]
+
+const rand = () => Math.floor(Math.random() * messages.length)
+
 
 const Loading = () => {
+
+	const [msj, setMsj] = useState(messages[rand()])
+
+	useEffect(() => {
+		setTimeout(() => {
+			let r = rand()
+			let new_msj = messages[r]
+			if( new_msj !== msj ){
+				setMsj(messages[r])
+				return
+			}
+			r++
+			r = r > messages.length ? 0 : r
+			setMsj(messages[r])
+		}, 3000)
+	}, [msj])
+
 	return (
 		<div className={ style.loading }>
-			<img src={ src } alt="Page loading" />
+			<div className={ style['loading-container'] }>
+				<img src={ src } alt="Page loading" />
+				<div className="">{ msj }</div>
+			</div>
 		</div>
 	)
 }
