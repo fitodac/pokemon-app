1f85ea0af1d1340e6d21f8372a3fa4b131da8e42
fitodac <fitodac@gmail.com>
2023-04-01 06:22:14 -0300

    Pagination navigation method changed

--- client/src/components/Pager/Pager.jsx
+++ client/src/components/Pager/Pager.jsx
@@ -1,31 +1,46 @@
-import { Link, useLocation } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { setUrlPager } from '../../utils/url'
+import { 
+	useDispatch, 
+	useSelector 
+} from 'react-redux'
+import { setPage } from '../../store/actions'
 
 import style from './Pager.module.css'
 
-const Pager = props => {
 
-	const location = useLocation()
-	const { 
-		page,
-		total_pages
-	} = props
+
+
+const Pager = props => {
+	
+	const dispatch = useDispatch()
+	const page = useSelector(state => state.page),
+				pages = useSelector(state => state.pages)
+	const history = useHistory()
+
+	const navigate = i => {
+		dispatch(setPage(i))
+		history.push( setUrlPager(i) )
+	}
 	
 
 	const items = []
-	for(let i = 1; i < total_pages + 1; i++){
+	for(let i = 1; i < pages + 1; i++){
 		items.push(
-			<Link 
-				to={`${location.pathname}?p=${i}`}
+			<button
+				onClick={ () => i === Number(page) ? null : navigate(i) } 
 				key={ `page-${i}` }
 				className={ i === Number(page) ? `${style.item} ${style.active}`: style.item }>
 				{i}
-			</Link>)
+			</button>)
 	}
+
+
 
 	return (
 		<div className={ style.pager }>
 			{ 
-				total_pages ? 
+				pages ? 
 				(<div className={ style.list }>
 					{ items }
 				</div>):
